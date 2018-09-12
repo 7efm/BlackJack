a7413beae4c8842f3e0361832e19f03cbde6a560
7efm <38299622+7efm@users.noreply.github.com>
2018-09-12 12:59:18 -0700

    BlackJack 9.12.18
    - Needs dealer's turn

--- BlackJack.java
+++ BlackJack.java
@@ -29,7 +29,7 @@ public class BlackJack
                 stop = true;
                 System.out.println("You stop playing and it is the dealers turn now.");
             }
-            
+
         }    
     }
 }
